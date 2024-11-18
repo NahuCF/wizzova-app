ca17af584d297f6c7bb5c66ab5c5a03ffde312d6
Nahuel <nahuefer173@gmail.com>
2024-11-17 21:43:28 -0300

    Base setup with primevue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,10 @@
 <template>
-  <main>test</main>
+  <main class="flex items-center justify-center">
+    <Button label="Button" />
+    <InputText></InputText>
+  </main>
 </template>
+
+<script setup lang="ts">
+import { Button, InputText } from 'primevue'
+</script>
