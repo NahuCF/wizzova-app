45ffbc73d070c824555e222304acdb86a8fb0244
Nahuel <nahuefer173@gmail.com>
2024-12-01 03:01:50 -0300

    Add toast to AuthLayout

--- src/app/main.ts
+++ src/app/main.ts
@@ -6,14 +6,20 @@ import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
 import MyPreset from './theme'
 import i18n from '~/config/i18n'
+import ToastService from 'primevue/toastservice'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from '../router'
 
 const app = createApp(App)
 
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+app.use(pinia)
 app.use(router)
+app.use(ToastService)
 app.use(i18n)
 app.use(PrimeVue, {
   theme: {
--- src/components/layouts/AuthLayout.vue
+++ src/components/layouts/AuthLayout.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="flex h-screen bg-slate-100">
     <RouterView />
+    <Toast position="bottom-right" />
   </div>
 </template>
 
 <script lang="ts" setup>
 import { RouterView } from 'vue-router'
+import { Toast } from 'primevue'
 </script>
