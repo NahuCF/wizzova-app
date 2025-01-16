9b97705d07bde131639a8a56c007f259faf4967d
Nahuel <nahuefer173@gmail.com>
2025-01-16 03:50:20 -0300

    Add conversation view

--- src/views/Conversations.vue
+++ src/views/Conversations.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>chats</h1>
-  <a
-    href="https://www.facebook.com/v16.0/dialog/oauth?client_id=2480654665657413&redirect_uri=http://localhost:5173/callback&scope=business_management,whatsapp_business_management"
-    >link</a
-  >
 </template>
+
+<script setup>
+import { Button } from 'primevue'
+</script>
