94057427623d53d0ed11a061039606c24f628786
Nahuel <nahuefer173@gmail.com>
2025-03-27 15:35:45 +0000

    feat: add scroll and padding to the main content

--- src/components/layouts/AppLayout.vue
+++ src/components/layouts/AppLayout.vue
@@ -3,9 +3,9 @@
     <TheSidebar />
     <RouterView v-slot="{ Component, route }">
       <transition name="fade" mode="out-in">
-        <div class="px-5 pt-3 w-full">
+        <main class="px-5 py-3 w-full overflow-y-scroll">
           <component :is="Component" :key="route.path" />
-        </div>
+        </main>
       </transition>
     </RouterView>
     <Toast position="bottom-right" />
