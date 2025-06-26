09ffba865ef0e390cf3e53268d666e8d795524b0
EzequielW <ezequielwokraczka@gmail.com>
2025-06-26 13:44:11 -0300

    fix: spinner overlay on both layouts

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+const isMounting = ref(true)
+
+onMounted(async () => {
+  const minDelay = new Promise(resolve => setTimeout(resolve, 500))
+  const renderReady = new Promise(requestAnimationFrame)
+
+  await Promise.all([minDelay, renderReady])
+  isMounting.value = false
+})
+</script>
+
 <template>
-  <RouterView />
+  <div>
+    <div v-if="isMounting" class="fixed inset-0 bg-white flex items-center justify-center z-150">
+      <ProgressSpinner style="width: 50px; height: 50px" strokeWidth="5" />
+    </div>
+    <RouterView />
+  </div>
 </template>
--- src/components/layouts/AppLayout.vue
+++ src/components/layouts/AppLayout.vue
@@ -1,36 +1,19 @@
 <script lang="ts" setup>
-import { onMounted, ref } from 'vue'
 import { RouterView } from 'vue-router'
-
-const isMounting = ref(true)
-
-onMounted(async () => {
-  const minDelay = new Promise(resolve => setTimeout(resolve, 500))
-  const renderReady = new Promise(requestAnimationFrame)
-
-  await Promise.all([minDelay, renderReady])
-  isMounting.value = false
-})
 </script>
 
 
 <template>
-  <div>
-    <div v-if="isMounting" class="fixed inset-0 bg-white flex items-center justify-center z-150">
-      <ProgressSpinner style="width: 50px; height: 50px" strokeWidth="5" />
-    </div>
-
-    <div class="flex h-screen bg-slate-100">
-      <TheSidebar />
-      <RouterView v-slot="{ Component, route }">
-        <transition name="fade" mode="out-in">
-          <main class="px-5 w-full overflow-y-scroll">
-            <component :is="Component" :key="route.path" />
-          </main>
-        </transition>
-      </RouterView>
-      <Toast position="bottom-right" />
-    </div>
+  <div class="flex h-screen bg-slate-100">
+    <TheSidebar />
+    <RouterView v-slot="{ Component, route }">
+      <transition name="fade" mode="out-in">
+        <main class="px-5 w-full overflow-y-scroll">
+          <component :is="Component" :key="route.path" />
+        </main>
+      </transition>
+    </RouterView>
+    <Toast position="bottom-right" />
   </div>
 </template>
 
