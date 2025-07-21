10e13824c97060e1e0968fcc9d13a175be4d2a19
Nahuel <nahuefer173@gmail.com>
2025-07-21 17:49:01 +0000

    chore: rename param

--- src/services/TenantService.ts
+++ src/services/TenantService.ts
@@ -3,7 +3,7 @@ import Http from '~/config/http'
 export default {
   async storeLongLovedToken(token: string = '') {
     return Http.post<{ data: any }>('/tenant/long-lived-token', {
-      token,
+      access_token: token,
     })
   },
 }
